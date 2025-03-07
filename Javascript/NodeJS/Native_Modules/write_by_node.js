dda91aaf26a4906982fd439bf9f052ceb66d90fe
aiien61 <aiien61will@gmail.com>
2025-03-07 15:17:41 +0800

    NodeJS practice

---
+++ Javascript/NodeJS/Native_Modules/write_by_node.js
@@ -0,0 +1,7 @@
+// fs for file system
+const fs = require("fs");
+
+fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+});
