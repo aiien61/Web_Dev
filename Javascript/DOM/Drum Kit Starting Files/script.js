6a960e9e78b133d659378a9b7ae48058f26e759e
aiien61 <aiien61will@gmail.com>
2025-06-28 15:41:47 +0800

    add keydown reaction to drum kit using object

--- Javascript/DOM/Drum Kit Starting Files/script.js
+++ Javascript/DOM/Drum Kit Starting Files/script.js
@@ -2,42 +2,53 @@ let buttonList = document.querySelectorAll(".drum");
 for (var i = 0; i < buttonList.length; i++) {
     buttonList[i].addEventListener("click", function () {
         let buttonInnerHTML = this.innerHTML;
-        let audio = new Audio(src="./sounds/crash.mp3");
-        switch (buttonInnerHTML) {
-            case "w":
-                audio = new Audio(src="./sounds/tom-1.mp3");
-                audio.play();
-                break;
-
-            case "a":
-                audio = new Audio(src="./sounds/tom-2.mp3");
-                audio.play();
-                break;
-
-            case "s":
-                audio = new Audio(src="./sounds/tom-3.mp3");
-                audio.play();
-                break;
-
-            case "d":
-                audio = new Audio(src="./sounds/tom-4.mp3");
-                audio.play();
-                break;
-
-            case "j":
-                audio = new Audio(src="./sounds/kick-bass.mp3");
-                audio.play();
-                break;
-
-            case "k":
-                audio = new Audio(src="./sounds/snare.mp3");
-                audio.play();
-                break;
-        
-            default:
-                audio.play();
-                break;
-        }
-
+        makeSound(buttonInnerHTML)
     });
+}
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let audioTom1 = new Audio(src="./sounds/tom-1.mp3");
+            audioTom1.play();
+            break;
+
+        case "a":
+            let audioTom2 = new Audio(src="./sounds/tom-2.mp3");
+            audioTom2.play();
+            break;
+
+        case "s":
+            let audioTom3 = new Audio(src="./sounds/tom-3.mp3");
+            audioTom3.play();
+            break;
+
+        case "d":
+            let audioTom4 = new Audio(src="./sounds/tom-4.mp3");
+            audioTom4.play();
+            break;
+
+        case "j":
+            let audioKick = new Audio(src="./sounds/kick-bass.mp3");
+            audioKick.play();
+            break;
+
+        case "k":
+            let audioSnare = new Audio(src="./sounds/snare.mp3");
+            audioSnare.play();
+            break;
+
+
+        case "l":
+            let audioCrash = new Audio(src="./sounds/crash.mp3");
+            audioCrash.play();
+            break;
+    
+        default:
+            break;
+    }
 }
--- Javascript/intro/objects.js
+++ Javascript/intro/objects.js
@@ -1,30 +1,36 @@
-let housekeeper = {
+let houseKeeper = {
     yearsOfExperience: 12,
     name: "Jane",
     cleaningRepertoire: ["bathroom", "lobby", "bedroom"]
 }
 
-console.log(housekeeper.name);
+console.log(houseKeeper.name);
 
 // constructor function
 function HouseKeeper(name, yearsOfExperience, cleaningRepertoire) {
     this.name = name,
     this.yearsOfExperience = yearsOfExperience,
-    this.cleaningRepertoire = cleaningRepertoire
+    this.cleaningRepertoire = cleaningRepertoire,
+    this.clean = function () {
+        console.log("Cleaning in progress...");
+    }
 }
 
-let housekeeper1 = new HouseKeeper("Jane", 12, ["bathroom", "lobby", "bedroom"]);
-console.log(housekeeper1.cleaningRepertoire);
+let houseKeeper1 = new HouseKeeper("Jane", 12, ["bathroom", "lobby", "bedroom"]);
+console.log(houseKeeper1.cleaningRepertoire);
+houseKeeper1.clean();
 
 // objects' methods
-function BellBoy(name, hasWorkPermit) {
+function BellBoy(name, age, hasWorkPermit, languages) {
     this.name = name,
+    this.age = age,
     this.hasWorkPermit = hasWorkPermit,
+    this.languages = languages,
     this.moveSuitcase = function () {
         console.log("Move the suitcase.")
     }
 }
 
-let bellboy1 = new BellBoy("Tom", true);
+let bellboy1 = new BellBoy("Tom", 20, true, ["English", "French"]);
 console.log(bellboy1.name);
 bellboy1.moveSuitcase();
