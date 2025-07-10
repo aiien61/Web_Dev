65d9e329fcdea2a28fad4632af23e50eef8b1c3d
aiien61 <aiien61will@gmail.com>
2025-07-10 16:15:11 +0800

    EJS tags

---
+++ Javascript/EJS/Tags/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+    const data = {
+        title: "EJS Tags",
+        seconds: new Date().getSeconds(),
+        items: ["apple", "banana", "cherry"],
+        htmlContent: "<strong>This is some strong text</strong>",
+    };
+    response.render("index.ejs", data);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
