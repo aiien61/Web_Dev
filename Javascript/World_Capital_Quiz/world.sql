990508e0230d1f42ed4c83abb65526a3592a0a16
aiien61 <aiien61will@gmail.com>
2025-09-23 15:43:34 +0800

    data and sql command history

---
+++ Javascript/World_Capital_Quiz/world.sql
@@ -0,0 +1,62 @@
+-- Create a table that stores a series of countries and their capital cities
+CREATE TABLE capitals (
+	id SERIAL PRIMARY KEY,
+	country VARCHAR(45),
+	capital VARCHAR(45)
+);
+
+-- Create a table that stores a series of countries and the emoji image of their national flags
+CREATE TABLE flags (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(45),
+	flag TEXT
+);
+
+-- Create a table that stores a series of countries and their rice production and wheat production
+CREATE TABLE world_food (
+	id SERIAL PRIMARY KEY,
+	country VARCHAR(45),
+	rice_production FLOAT,
+	wheat_production FLOAT
+);
+
+SELECT country FROM world_food;
+
+SELECT country, wheat_production FROM world_food;
+
+-- Find the rice production of the USA
+SELECT rice_production 
+FROM world_food
+WHERE country='United States';
+
+-- Find all the countries that have wheat production greater than 20
+SELECT country
+FROM world_food
+WHERE wheat_production>20;
+
+-- Find all the countries that have 'U' as the beginning letter in their country name
+SELECT country
+FROM world_food
+WHERE country LIKE 'U' || '%';
+
+-- Find all the countries that have 'a' as the ending letter in their country name
+SELECT country
+FROM world_food
+WHERE country LIKE '%' || 'a';
+
+-- Create a table that records a series of visited countries
+CREATE TABLE visited_countries(
+	id SERIAL PRIMARY KEY,
+	country_code CHAR(2) NOT NULL UNIQUE
+);
+
+-- Insert a new record to world_food table
+INSERT INTO world_food (country, rice_production, wheat_production)
+VALUES ('Italy', 1.46, 7.3);
+
+-- Create a table that stores countries and their codes
+CREATE TABLE countries(
+	id SERIAL PRIMARY KEY,
+	country_code CHAR(2),
+	country_name VARCHAR(100)
+);
